8bfebd14875188290387062ad7ecea097ddcaba1
Vũ Cường <vucuong@Vus-MacBook-Pro-2.local>
2019-12-15 20:19:25 +0700

    Updates

---
+++ precache-manifest.56dc4df45b81ac9ed401c300b791c221.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72a7043047c6a5c2a595a933675bc66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "493692c7d9ae52274aa9",
+    "url": "/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "2acaf580d25b361c299a",
+    "url": "/static/css/main.220dd7ce.chunk.css"
+  },
+  {
+    "revision": "493692c7d9ae52274aa9",
+    "url": "/static/js/2.44d2e00a.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/static/js/2.44d2e00a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66432cde0bc2fdb4b890",
+    "url": "/static/js/3.ed57f20e.chunk.js"
+  },
+  {
+    "revision": "2acaf580d25b361c299a",
+    "url": "/static/js/main.8aa275ee.chunk.js"
+  },
+  {
+    "revision": "c775709af1f859968a8f",
+    "url": "/static/js/runtime-main.00043366.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
